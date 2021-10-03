bb52b5989a6578421eb7f51368fb36b85a1ee3a6
Vasili888-QA <vasil88888@inbox.ru>
2021-10-03 15:41:09 +0300

    Lesson -10- Fix - Add class Attach for video and screenshots

--- src/test/java/parameterizeTests/TestBase.java
+++ src/test/java/parameterizeTests/TestBase.java
@@ -6,6 +6,7 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import parameterizeTests.helper.Attach;
 
 public class TestBase {
     @BeforeAll
@@ -21,11 +22,11 @@ public class TestBase {
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
     }
 
-//    @AfterEach
-//    public void tearDown() {
-//        Attach.screenshotAs("Last screenshot");
-//        Attach.pageSource();
-//        Attach.browserConsoleLogs();
-//        Attach.addVideo();
-//    }
+    @AfterEach
+    public void tearDown() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+    }
 }
