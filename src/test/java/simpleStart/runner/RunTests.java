2e3f8a276d2ef750569bac1ec6a7a86f84bfed1d
Vasili888-QA <vasil88888@inbox.ru>
2021-10-16 19:20:20 +0300

    class RunTests by Cucumber extends TestBase

--- src/test/java/simpleStart/runner/RunTests.java
+++ src/test/java/simpleStart/runner/RunTests.java
@@ -3,6 +3,7 @@ package simpleStart.runner;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
+import parameterizeTests.TestBase;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
@@ -10,6 +11,6 @@ import org.junit.runner.RunWith;
         tags = "@Test or @Story or @Lesson1 or @Lesson2 or @Lesson4 or @Lesson8",
         glue = {"simpleStart/stepDefinitions"},
         plugin = {"pretty", "io.qameta.allure.cucumber6jvm.AllureCucumber6Jvm"})
-public class RunTests {
+public class RunTests extends TestBase {
 }
 
